5b5089eb44fcf78d04c83590ec50cead2ba2c889
Ramzi-Theo <99851487+Ramzi-Theo@users.noreply.github.com>
2022-10-07 11:30:08 +0200

    Update Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -21,7 +21,7 @@ const Posts=()=>{
       <CardMedia
         component="img"
         height="190"
-        image="components/booked.png"
+        image="booked.png"
         alt="Marks" />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
